5ee66010e8664b97e7b7a625a6dfa425362a80af
Eric Joel Marte <115370460+ericjoel25@users.noreply.github.com>
2022-10-19 20:44:00 -0400

    Update firebase.js

--- src/component/firebase/firebase.js
+++ src/component/firebase/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBDQNmkhZ1rg-G3Aeaa3dfkMJFEBj1U-oU",
-    authDomain: "prestamos-jj.firebaseapp.com",
-    databaseURL: "https://prestamos-jj-default-rtdb.firebaseio.com",
-    projectId: "prestamos-jj",
-    storageBucket: "prestamos-jj.appspot.com",
-    messagingSenderId: "496989944627",
-    appId: "1:496989944627:web:fd1e19cf6fc6f7ae215c46"
+    apiKey: "****************",
+    authDomain:  "****************",
+    databaseURL: "****************",
+    projectId: "****************",
+    storageBucket:  "****************",
+    messagingSenderId: "****************",
+    appId:  "****************",
   
   };
 
